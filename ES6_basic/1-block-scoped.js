36d950138a4154bda5f260862261a8e5992ff080
kdn95 <khangdn95@gmail.com>
2024-07-11 15:18:05 +1000

    added task up to 3

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) {
+  var task;
+  var task2; 
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  task = false;
+  task2 = true;
+  
+  return [task, task2];
+}
---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  //removed "function add(...)"
+  //"add" removed because unnecessary
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
