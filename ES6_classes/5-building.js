5070d8492e450ccb0916dc33c3cac68634c2b9d1
kdn95 <khangdn95@gmail.com>
2024-07-13 18:58:44 +1000

    added task 6

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,17 +3,17 @@ export default class Building {
   //create object from this
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building) {
+    if (new.target === Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     };
   }
 
   get sqft() {
-    return `${this._sqft}`;
+    return this._sqft;
   }
   
   evacuationWarningMessage() {
-    if (this.constructor !== Building) {
+    if (new.target == Building) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     };
   }
