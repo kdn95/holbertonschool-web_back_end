8c670e6d156cd95a324b439661fe90d72a6021fc
kdn95 <khangdn95@gmail.com>
2024-07-15 16:59:16 +1000

    added 2

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(x, y) {
+  // Filter element funct
+  // Element funct returns element.location equal to
+  // arg "y"
+  const name = x.filter(element => element.location === y);
+  return name;
+}
