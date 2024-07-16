53351e3d50ed453101ce4ffc09d9d04b4998b957
kdn95 <khangdn95@gmail.com>
2024-07-16 16:15:56 +1000

    added task 10:

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(item) {
+  // if item is not even a Map, throw error
+  if (!item instanceof Map) {
+    throw new Error('Cannot process');
+  }
+  // getting all values in item map
+  const theList = [...item];
+  // for each key:value pair of spread item list
+  for (const [key,value] of theList) {
+    // if value is 1
+    if (value === 1) {
+      // set value based on key in item list to 100
+      item.set(key, 100);
+    }
+  }
+}
---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList(name, quantity) {
+  const theGroceries = new Map();
+  
+  theGroceries.set('Apples', 10);
+  theGroceries.set('Tomatoes', 10);
+  theGroceries.set('Pasta', 1);
+  theGroceries.set('Rice', 1);
+  theGroceries.set('Banana', 5);
+
+  return theGroceries;
+ }
