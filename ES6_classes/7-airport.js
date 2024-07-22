acf04b103625fdde12f522d59dc1813add1c80a3
kdn95 <khangdn95@gmail.com>
2024-07-22 15:24:22 +1000

    updated task 7

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,3 +1,5 @@
+// util = is found in the node module folder
+// require() grabs the input from Node modules
 const util = require('util');
 
 export default class Airport {
@@ -6,7 +8,8 @@ export default class Airport {
     this._code = code;
     
   }
-  // have no idea why util.inspect.custom is used (did not learn)
+  // have no idea why util.inspect.custom is used (did not learn) 15/07/2024
+  // 
   [util.inspect.custom]() {
     return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}'}`;
   }
