afccb7fc5b464ca59afbaa2721bb3d94e6affef5
kdn95 <khangdn95@gmail.com>
2024-07-23 10:53:48 +1000

    updated 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,19 +1,31 @@
+// export default function handleResponseFromAPI(promise) {
+//   const ThePromise = new Promise((resolve, reject) => {
+//     if (promise === true) {
+//       resolve({status: 200, body: 'Success',
+//       });
+//     } else {
+//       reject(Error({}));
+//     }
+//   });
+// 
+//   // const resp = 'Got a response from the API'
+// 
+//   ThePromise.then(function(resolve) {
+//     console.log('Got a response from the API');
+//   }, function(reject) {
+//     console.log('Got a response from the API');
+//   });
+// 
+// }
+
 export default function handleResponseFromAPI(promise) {
-  const ThePromise = new Promise((resolve, reject) => {
-    if (promise === true) {
-      resolve({status: 200, body: 'Success',
-      });
-    } else {
-      reject(Error({}));
-    }
-  });
-
-  // const resp = 'Got a response from the API'
-
-  ThePromise.then(function(resolve) {
-    console.log('Got a response from the API');
-  }, function(reject) {
-    console.log('Got a response from the API');
-  });
-
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
