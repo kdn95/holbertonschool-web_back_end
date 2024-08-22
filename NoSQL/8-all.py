df92fa038702dfb3d9d64800799fafe13d0633ba
kdn95 <khangdn95@gmail.com>
2024-08-22 11:04:03 +1000

    added 6-8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection) -> list:
+    """ Py function listing all documents in collection """
+    listing = []
+
+    # da_client = MongoClient("mongodb://127.0.0.1:27017")
+    # da_db = da_client["my_db"]
+    # da_collection = da_db["school"]
+    # return empty list if no docs in collection
+    # if mongo_collection is None:
+        # return []
+    # loop in collection for all docs using find()
+    # find() returns all occurrence in collection
+    for x in mongo_collection.find():
+        listing.append(x)
+
+    return listing
